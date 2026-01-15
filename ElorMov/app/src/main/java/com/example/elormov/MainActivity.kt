a46a0d3b10c34aa3297b791dd397ec5ad66267cc
Akira Sakakibara Ruiz <akira.sakakibararu@elorrieta-errekamari.com>
2026-01-15 16:13:48 +0100

    Creados parte de los componentes del Login a falta de la imagen y los logos. Tienen un popup de recuperacion de contraseña al que hay que darle funcionalidad. Falta toda la lógica.

--- ElorMov/app/src/main/java/com/example/elormov/MainActivity.kt
+++ ElorMov/app/src/main/java/com/example/elormov/MainActivity.kt
@@ -3,22 +3,57 @@ package com.example.elormov
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val boton : Button = findViewById(R.id.buttonMainAceptar)
+        val btnAceptar : Button = findViewById(R.id.buttonMainAceptar)
+        val btnRecuperar = findViewById<Button?>(R.id.buttonRecuperarPassword)
 
-        boton.setOnClickListener {
+        btnRecuperar.setOnClickListener {
+            popUpRecuperarContrasenna()
+        }
+
+        btnAceptar.setOnClickListener {
             val intent = Intent(this, PerfilActivity::class.java)
             startActivity(intent)
             finish()
         }
+    }
+
+    private fun popUpRecuperarContrasenna() {
+
+        val inputEmail = findViewById<TextInputEditText>(R.id.InputEmail)
+        val email = inputEmail.text.toString()
+
+        val mensaje = if (email.isNotEmpty()) {
+            "$email, ${getString(R.string.recuperarPasswordPregunta)}"
+        } else {
+            getString(R.string.recuperarPasswordPregunta)
+        }
+
+        MaterialAlertDialogBuilder(this)
+            .setTitle(getString(R.string.recuperarPassword))
+            .setMessage(mensaje)
+            .setPositiveButton(R.string.si) { _, _ ->
+
+                //Aqui va el codigo relacionado con lo de recuperar la contraseña
+                Toast.makeText(
+                    this,
+                    R.string.enviarNuevoPassword,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            .setNegativeButton(R.string.no) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 }
