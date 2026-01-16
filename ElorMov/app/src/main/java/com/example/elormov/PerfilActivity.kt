cb158b005eb59d5eef5bb90b157e9d35235993df
Akira Sakakibara Ruiz <akira.sakakibararu@elorrieta-errekamari.com>
2026-01-16 21:14:15 +0100

    Pequeños cambios sin importancia

--- ElorMov/app/src/main/java/com/example/elormov/PerfilActivity.kt
+++ ElorMov/app/src/main/java/com/example/elormov/PerfilActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
 class PerfilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
